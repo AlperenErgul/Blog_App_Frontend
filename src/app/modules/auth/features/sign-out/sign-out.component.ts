d0ba4a46871903cfe9e87be4407eba2f1ccc502b
AlperenErgul <alperenergul49@gmail.com>
2025-02-16 00:33:01 +0300

    chore: auth pages design is done

--- src/app/modules/auth/features/sign-out/sign-out.component.ts
+++ src/app/modules/auth/features/sign-out/sign-out.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sign-out',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
   standalone: true,
   styleUrl: './sign-out.component.css'
 })
-export class SignOutComponent {
+export class SignOutComponent implements OnInit{
+
+  countdown: number = 3;
+  countdownInterval: any;
+
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.startCountdown();
+  }
+
+  startCountdown(): void {
+    this.countdownInterval = setInterval(() => {
+      if (this.countdown > 1) {
+        this.countdown--;
+        document.getElementById('countdown')!.innerText = this.countdown.toString();
+      } else {
+        clearInterval(this.countdownInterval);
+        this.router.navigate(['/sign-in']);
+      }
+    }, 1000);
+  }
 
 }
