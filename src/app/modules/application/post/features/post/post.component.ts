9b515dc806b17b921aa636bf622c1625d1a04914
AlperenErgul <alperenergul49@gmail.com>
2025-02-16 23:53:18 +0300

    chore: data transfer from smart to dump added

--- src/app/modules/application/post/features/post/post.component.ts
+++ src/app/modules/application/post/features/post/post.component.ts
@@ -16,4 +16,83 @@ import {PostListComponent} from '../../ui/post-list/post-list.component';
 })
 export class PostComponent {
 
+  posts = [
+    {
+      username: 'Alperen Ergül',
+      createdAt: '07.11.2022',
+      title: 'Tailwind & Angular',
+      content: 'Tailwind CSS ve Angular kullanarak harika arayüzler oluşturuyorum. 🚀'
+    },
+    {
+      username: 'John Doe',
+      createdAt: '12.05.2023',
+      title: 'Frontend Geliştirme',
+      content: 'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.' +
+        'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.' +
+        'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.' +
+        'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.' +
+        'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.'
+    },
+    {
+      username: 'Jane Smith',
+      createdAt: '28.09.2023',
+      title: 'TypeScript ve Angular',
+      content: 'TypeScript’in sağladığı tip güvenliği ile Angular uygulamalarını daha sağlam hale getiriyoruz.'
+    },
+    {
+      username: 'Alperen Ergül',
+      createdAt: '07.11.2022',
+      title: 'Tailwind & Angular',
+      content: 'Tailwind CSS ve Angular kullanarak harika arayüzler oluşturuyorum. 🚀'
+    },
+    {
+      username: 'John Doe',
+      createdAt: '12.05.2023',
+      title: 'Frontend Geliştirme',
+      content: 'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.'
+    },
+    {
+      username: 'Jane Smith',
+      createdAt: '28.09.2023',
+      title: 'TypeScript ve Angular',
+      content: 'TypeScript’in sağladığı tip güvenliği ile Angular uygulamalarını daha sağlam hale getiriyoruz.'
+    },
+    {
+      username: 'Alperen Ergül',
+      createdAt: '07.11.2022',
+      title: 'Tailwind & Angular',
+      content: 'Tailwind CSS ve Angular kullanarak harika arayüzler oluşturuyorum. 🚀'
+    },
+    {
+      username: 'John Doe',
+      createdAt: '12.05.2023',
+      title: 'Frontend Geliştirme',
+      content: 'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.'
+    },
+    {
+      username: 'Jane Smith',
+      createdAt: '28.09.2023',
+      title: 'TypeScript ve Angular',
+      content: 'TypeScript’in sağladığı tip güvenliği ile Angular uygulamalarını daha sağlam hale getiriyoruz.'
+    },
+    {
+      username: 'Alperen Ergül',
+      createdAt: '07.11.2022',
+      title: 'Tailwind & Angular',
+      content: 'Tailwind CSS ve Angular kullanarak harika arayüzler oluşturuyorum. 🚀'
+    },
+    {
+      username: 'John Doe',
+      createdAt: '12.05.2023',
+      title: 'Frontend Geliştirme',
+      content: 'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.'
+    },
+    {
+      username: 'Jane Smith',
+      createdAt: '28.09.2023',
+      title: 'TypeScript ve Angular',
+      content: 'TypeScript’in sağladığı tip güvenliği ile Angular uygulamalarını daha sağlam hale getiriyoruz.'
+    }
+  ];
+
 }
--- src/app/modules/application/post/ui/post-list/post-list.component.ts
+++ src/app/modules/application/post/ui/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-ui-post-list',
@@ -9,87 +9,7 @@ import {Component} from '@angular/core';
 })
 export class PostListComponent {
 
-  posts = [
-    {
-      username: 'Alperen Ergül',
-      createdAt: '07.11.2022',
-      title: 'Tailwind & Angular',
-      content: 'Tailwind CSS ve Angular kullanarak harika arayüzler oluşturuyorum. 🚀'
-    },
-    {
-      username: 'John Doe',
-      createdAt: '12.05.2023',
-      title: 'Frontend Geliştirme',
-      content: 'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.' +
-        'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.' +
-        'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.' +
-        'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.' +
-        'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.'
-    },
-    {
-      username: 'Jane Smith',
-      createdAt: '28.09.2023',
-      title: 'TypeScript ve Angular',
-      content: 'TypeScript’in sağladığı tip güvenliği ile Angular uygulamalarını daha sağlam hale getiriyoruz.'
-    },
-    {
-      username: 'Alperen Ergül',
-      createdAt: '07.11.2022',
-      title: 'Tailwind & Angular',
-      content: 'Tailwind CSS ve Angular kullanarak harika arayüzler oluşturuyorum. 🚀'
-    },
-    {
-      username: 'John Doe',
-      createdAt: '12.05.2023',
-      title: 'Frontend Geliştirme',
-      content: 'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.'
-    },
-    {
-      username: 'Jane Smith',
-      createdAt: '28.09.2023',
-      title: 'TypeScript ve Angular',
-      content: 'TypeScript’in sağladığı tip güvenliği ile Angular uygulamalarını daha sağlam hale getiriyoruz.'
-    },
-    {
-      username: 'Alperen Ergül',
-      createdAt: '07.11.2022',
-      title: 'Tailwind & Angular',
-      content: 'Tailwind CSS ve Angular kullanarak harika arayüzler oluşturuyorum. 🚀'
-    },
-    {
-      username: 'John Doe',
-      createdAt: '12.05.2023',
-      title: 'Frontend Geliştirme',
-      content: 'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.'
-    },
-    {
-      username: 'Jane Smith',
-      createdAt: '28.09.2023',
-      title: 'TypeScript ve Angular',
-      content: 'TypeScript’in sağladığı tip güvenliği ile Angular uygulamalarını daha sağlam hale getiriyoruz.'
-    },
-    {
-      username: 'Alperen Ergül',
-      createdAt: '07.11.2022',
-      title: 'Tailwind & Angular',
-      content: 'Tailwind CSS ve Angular kullanarak harika arayüzler oluşturuyorum. 🚀'
-    },
-    {
-      username: 'John Doe',
-      createdAt: '12.05.2023',
-      title: 'Frontend Geliştirme',
-      content: 'Component tabanlı geliştirme yaparak modüler bir yapı oluşturuyoruz.'
-    },
-    {
-      username: 'Jane Smith',
-      createdAt: '28.09.2023',
-      title: 'TypeScript ve Angular',
-      content: 'TypeScript’in sağladığı tip güvenliği ile Angular uygulamalarını daha sağlam hale getiriyoruz.'
-    }
-  ];
-
-  constructor() {
-  }
+  @Input() posts: any[] = [];
 
 
 
